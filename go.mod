e23430efc43a2e061f555fb467efb390c2ab9508
Mingyuan Li <m674li@uwaterloo.ca>
2023-05-10 11:46:20 -0500

    added aes-256-cbc encryption & decrytion

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/GadzeFinance/etherfi-sync-clientv2
 
 go 1.20
+
+require golang.org/x/crypto v0.9.0 // indirect
---
+++ tests/decrypt.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto')
+
+function decrypt(text, ENCRYPTION_KEY) {
+
+  let textParts = text.split(':');
+  let iv = Buffer.from(textParts.shift(), 'hex');
+  let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText);
+
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+  return decrypted.toString();
+}
+
+const key = "6368616e676520746869732070617373"
+const text = "exampleplaintext"
+console.log(decrypt(text, key))
